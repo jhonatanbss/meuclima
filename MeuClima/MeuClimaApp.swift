7f9524a2832124ce0455d15d58965375de49e80b
Jhonatanbss <jhonatanbruno7@hotmail.com>
2022-06-14 11:33:06 -0300

    Initial Commit

---
+++ MeuClima/MeuClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeuClimaApp.swift
+//  MeuClima
+//
+//  Created by Jhonatan Bruno Santos Silva on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MeuClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
